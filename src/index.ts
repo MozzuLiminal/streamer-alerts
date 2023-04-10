aa27cb1fd8b3d0341bfd5058942df1808f98bd30
MozzuLiminal <130315136+MozzuLiminal@users.noreply.github.com>
2023-04-10 22:15:06 +0200

    start the twitch platform service

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-const awd = () => {
-  console.log('awd');
-};
+import dotenv from 'dotenv';
+import express from 'express';
+import { Twitch } from './platforms/twitch';
+import db from './services/db';
+
+const app = express();
+
+dotenv.config();
+
+const twitch = new Twitch();
+
+const tokens = twitch.TOKEN_NAMES.reduce<Record<string, string | undefined>>((acc, token) => {
+  return { ...acc, [token]: process.env[token] };
+}, {});
+
+twitch.init(tokens, db).then(() => console.log('twitch has inited'));
+twitch.registerWebhooks(app);
+
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+
+app.listen(3000, () => {
+  console.log('express is listening to 3000');
+});
