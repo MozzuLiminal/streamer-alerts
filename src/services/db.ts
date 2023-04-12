07ff7efdc704deba725ef59dcd038811c8275cf7
MozzuLiminal <130315136+MozzuLiminal@users.noreply.github.com>
2023-04-12 14:29:12 +0200

    use a callback strategy instead

--- src/services/db.ts
+++ src/services/db.ts
@@ -5,7 +5,7 @@ import path from 'node:path';
 const filepath = path.join(__dirname, '..', '..', 'db.json');
 
 export interface Database {
-  set: (object: Record<string, any>) => Promise<void>;
+  set: (callback: (data: Record<string, string>) => Record<string, string>) => Promise<void>;
   get: () => Promise<Record<string, any>>;
 }
 
@@ -13,10 +13,12 @@ if (!fsSync.existsSync(filepath)) {
   fs.writeFile(filepath, '{}', 'utf-8');
 }
 
-export const set: Database['set'] = async (object) => {
+export const set: Database['set'] = async (callback) => {
   const existingData = JSON.parse(await fs.readFile(filepath, 'utf-8'));
 
-  fs.writeFile(filepath, JSON.stringify({ ...existingData, ...object }, null, 4), 'utf-8');
+  const newData = callback(existingData);
+
+  fs.writeFile(filepath, JSON.stringify(newData, null, 4), 'utf-8');
 };
 
 export const get: Database['get'] = async () => JSON.parse(await fs.readFile(filepath, 'utf-8'));
