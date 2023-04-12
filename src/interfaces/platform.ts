414c97042d2e732e25472d740b23a6d6b6957063
MozzuLiminal <130315136+MozzuLiminal@users.noreply.github.com>
2023-04-12 14:31:26 +0200

    updated the platform interface

--- src/interfaces/platform.ts
+++ src/interfaces/platform.ts
@@ -1,12 +1,21 @@
-import { SlashCommandBuilder } from 'discord.js';
 import { Express } from 'express';
+import TypedEventEmitter from 'typed-emitter';
 import { Database } from '../services/db';
+
+export type PlatformEvents = TypedEventEmitter<{
+  online: (name: string) => void;
+}>;
 
 export interface Platform {
   name: string;
   description: string;
   TOKEN_NAMES: string[];
-  commands: () => SlashCommandBuilder[];
+  events: PlatformEvents;
   registerWebhooks: (express: Express) => void;
   init: (tokens: Record<string, string | undefined>, database: Database) => Promise<void>;
+  addStreamerAlert: (name: string) => Promise<boolean>;
+  removeStreamerAlert: (name: string) => boolean;
+  isStreamerSubscribed: (name: string) => boolean;
+  formatURL: (username: string) => string;
+  close: () => void;
 }
